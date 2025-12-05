4e9952f6ac78e89a7c32c7ac833fddd4c1e233f8
Quang Hổ <trinhvu24689@gmail.com>
2025-12-05 16:47:16 +0700

    add db_engine for fly

--- db_engine.py
+++ db_engine.py
@@ -1,23 +1,27 @@
-from sqlalchemy import create_engine, Column, String, Integer, DateTime
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
 from sqlalchemy.orm import sessionmaker, declarative_base
-import datetime
+import datetime as dt
+import os
 
-DATABASE_URL = "sqlite:///./swap.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./swap.db")
 
 engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL,
+    connect_args={"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
 )
 
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
 
 class SwapHistoryModel(Base):
     __tablename__ = "swap_history"
 
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(String)
-    image_path = Column(String)
-    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    user_id = Column(String, index=True)
+    file_name = Column(String)
+    created_at = Column(DateTime, default=dt.datetime.utcnow)
+
 
 def init_swap_db():
     Base.metadata.create_all(bind=engine)
