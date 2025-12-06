0ccc4b3b214ddc2506882801f256c20adb82acec
Quang Hổ <trinhvu24689@gmail.com>
2025-12-06 14:50:13 +0700

    fix CORS for vercel

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ app = FastAPI(title="FaceSwap AI Backend", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[""https://zenithswap.vercel.app""],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
